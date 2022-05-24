86b4c3f7685c30587875723ac91c5ce6dcd6f47d
Lester Claudio <claudiol@redhat.com>
2022-05-24 09:53:28 -0600

    Fixed inconsistent spacings (line 20, 28, 30, 33, 35)

--- scripts/generate-subscriptions.sh
+++ scripts/generate-subscriptions.sh
@@ -10,34 +10,34 @@
 function generateSubscriptionEntry {
   for i in $subscriptions
   do
-  	PACKNAME=$(oc get packagemanifests | grep -i $i | awk '{ print $1}')
-	if [ ".$PACKNAME" == "." ]; then
-		echo "Package: $i unknown"
-		continue
- 	fi
-  	for j in $PACKNAME
-  	do 
-          echo "Package Info for [$j]"
-  	  OUT=$(oc get packagemanifest/$j -o json | jq '.metadata.name, .metadata.namespace, .metadata.labels.catalog, .status.defaultChannel, .status.channels[].currentCSV')
-  	  let COUNT=1
-  	  for k in $OUT
-  	  do
-  	    case $COUNT in 
-  	  	1) echo "    $k:" | tr -d '"'
-  		   echo "      name: $k" | tr -d '"'
-  	    	   ;;
-  	  	2) echo "      namespace: $k" | tr -d '"'
-  	    	   ;;
-  	   	3) echo "      source: $k" | tr -d '"'
-  		   ;;
-     		4) echo "      channel: $k" | tr -d '"'
-  		   ;;
-     		5) echo "      csv: $k" | tr -d '"'
-  		   ;;
-  	    esac
-  	    let COUNT++
-  	  done
-  	done
+  PACKNAME=$(oc get packagemanifests | grep -i $i | awk '{ print $1}')
+if [ ".$PACKNAME" == "." ]; then
+echo "Package: $i unknown"
+continue
+ fi
+  for j in $PACKNAME
+  do 
+    echo "Package Info for [$j]"
+    OUT=$(oc get packagemanifest/$j -o json | jq '.metadata.name, .metadata.namespace, .metadata.labels.catalog, .status.defaultChannel, .status.channels[].currentCSV')
+    let COUNT=1
+    for k in $OUT
+    do
+      case $COUNT in 
+      1) echo "    $k:" | tr -d '"'
+         echo "      name: $k" | tr -d '"'
+         ;;
+      2) echo "      namespace: $k" | tr -d '"'
+         ;;
+      3) echo "      source: $k" | tr -d '"'
+         ;;
+      4) echo "      channel: $k" | tr -d '"'
+         ;;
+      5) echo "      csv: $k" | tr -d '"'
+         ;;
+      esac
+      let COUNT++
+    done
+  done
   done
 }
 

e37b6125f577bf90e4d8703c2275a6754ee9ae02
Lester Claudio <claudiol@redhat.com>
2023-02-22 13:34:56 -0700

    Adding script to restart DEX pods when not responding

---
+++ scripts/fix-dex.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+#
+
+function log {
+    if [ -z "$2" ]; then
+        echo -e "\033[0K\r\033[1;36m$1\033[0m"
+    else
+        echo -e $1 "\033[0K\r\033[1;36m$2\033[0m"
+    fi
+}
+
+log -n "Retirevng namespaces for OpenShift GitOps DEX pods ... "
+DEXNAMESPACES=$(oc get pods -A | grep -i dex | awk '{print $1}')
+log "Retirevng namespaces for OpenShift GitOps DEX pods ... ok"
+
+for namespace in $DEXNAMESPACES; do
+  DEXPOD=$(oc get pods -n $namespace | grep dex | awk '{ print $1 }')
+  log -n "Deleting pod [ $DEXPOD ] in namespace [ $namespace ] ... "
+  RC=$(oc delete -n $namespace pod/$DEXPOD;echo $? > /tmp/rc)
+  if [ $(cat /tmp/rc) -eq 0 ]; then
+    log "Deleting pod [ $DEXPOD ] in namespace [ $namespace ] ... ok"
+  else
+    log "Deleting pod [ $DEXPOD ] in namespace [ $namespace ] ... failed"
+  fi
+done

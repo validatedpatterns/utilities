7288556021ee9b54c19822425a9400e6e373ffd5
Michele Baldessari <michele@acksyn.org>
2023-07-24 14:49:08 +0900

    Add functionality to push each chart to github

--- helm-migration/migrate-common.sh
+++ helm-migration/migrate-common.sh
@@ -3,9 +3,11 @@ set -e -o pipefail
 
 CHARTS="clustergroup acm hashicorp-vault golang-external-secrets"
 COMMON="https://github.com/hybrid-cloud-patterns/common/"
+GITBASE_URL=""
 
 function usage {
   echo "Run: ${0} [-h|--help] -d|--destinationdir <folder in which to create all the chart repositories>"
+  echo "          [-g|--gitbaseurl] <gitbase url to use to push repos out>"
   echo "          [-t|--templatedir <folder where optional github workflow templates are stored>]"
   echo ""
   echo "Reads common/ and splits of all the helm charts as separate git repos"
@@ -14,10 +16,11 @@ function usage {
   echo "Usage:"
   echo "    -h|--help                   - Optional. Prints this help page"
   echo "    -d|--destinationdir <dir>   - Mandatory. In which folder to create the helm chart git repos. Must not exist"
+  echo "    -g|--gitbaseurl <name>      - Optional. When set it pushes out each chart to that org. E.g. 'git@github.com:mbaldessari/' will push to each repo like 'mbaldessari/acm-chart' etc"
   echo "    -t|--templatedir <dir>      - Optional. When specified it copies all the workflows contained in .github/workflow/"
 }
 
-if ! getopt -o hd:t: -l help,destinationdir:,templatedir:; then
+if ! getopt -o hd:t:g: -l help,destinationdir:,templatedir:,gitbaseurl:; then
     # Error, getopt will put out a message for us
     usage
     exit 1
@@ -36,6 +39,10 @@ while [ $# -gt 0 ]; do
       ;;
     -t|--templatedir)
       TEMPLATE_DIR="$2"
+      shift
+      ;;
+    -g|--gitbaseurl)
+      GITBASE_URL="$2"
       shift
       ;;
     *)
@@ -71,11 +78,27 @@ for i in ${CHARTS}; do
     if [ -n "${TEMPLATE_DIR}" ]; then
         echo "Copying templates..."
         mkdir -p .github/workflows
-        cp -avf "${TEMPLATE_DIR}/.github/workflows/*" .github/workflows
+        cp -avf "${TEMPLATE_DIR}/.github/workflows/"* .github/workflows
         git add .github/workflows
         git commit -a -m "Added .github/workflows for helm chart"
     fi
     popd
 done
 popd # common
+# Remove common
+rm -rf ./common
+
+# Push out the repos to the respective repositories
+if [ -z "${GITBASE_URL}" ]; then
+    echo "No gitbaseurl set. Exiting here"
+    exit 0
+fi
+
+for i in $(ls -d *); do
+    pushd "${i}"
+    git remote add origin "${GITBASE_URL}/${i}.git"
+    git branch -M main
+    git push -u origin main
+    popd
+done
 popd # dest_dir

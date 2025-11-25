6953312833d9a156b0e81c4d2ce526d422c85775
Michele Baldessari <michele@acksyn.org>
2025-11-25 09:47:55 +0100

    More reformatting

--- aws-tools/s3-create.py
+++ aws-tools/s3-create.py
@@ -8,78 +8,84 @@ import getopt
 import os
 import json
 
-regions = ['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2']
+regions = ["us-west-1", "us-west-2", "us-east-1", "us-east-2"]
 
 try:
-    aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+    aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
 except KeyError:
-    print("Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables for AWS access.")
+    print(
+        "Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables for AWS access."
+    )
     exit()
 
 if not aws_access_key_id or not aws_secret_access_key:
-    print("AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables cannot be empty.")
+    print(
+        "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables cannot be empty."
+    )
     exit()
 
 argumentList = sys.argv[1:]
 options = "hpb:r:"
 long_options = ["help", "bucket", "region", "public"]
 
+
 def usage():
-    msg = ("Usage: python " + sys.argv[0] + "\n"
-           "Options: \n"
-           " -b or --bucket \n"
-           "    S3 bucket name to create \n"
-           "    Example: -b my-bucket \n"
-           " -r or --region \n"
-           "    AWS region for bucket \n"
-           "    Example: -r us-west-1 \n"
-           " -h or --help \n"
-           "    Usage message")
+    msg = (
+        "Usage: python " + sys.argv[0] + "\n"
+        "Options: \n"
+        " -b or --bucket \n"
+        "    S3 bucket name to create \n"
+        "    Example: -b my-bucket \n"
+        " -r or --region \n"
+        "    AWS region for bucket \n"
+        "    Example: -r us-west-1 \n"
+        " -h or --help \n"
+        "    Usage message"
+    )
     print(msg)
     exit()
+
 
 def get_s3_client(region):
     endpoint_url = f"https://s3.{region}.amazonaws.com"
     try:
         s3 = boto3.client(
-            's3',
+            "s3",
             endpoint_url=endpoint_url,
             aws_access_key_id=aws_access_key_id,
             aws_secret_access_key=aws_secret_access_key,
             region_name=region,
-            config=botocore.client.Config(signature_version='s3v4')
+            config=botocore.client.Config(signature_version="s3v4"),
         )
         return s3
     except ClientError:
         print("Could not retrieve S3 client.")
         raise
 
+
 def create_bucket(s3_client, bucket_name, region, public=None):
     try:
         # For us-east-1, do not include CreateBucketConfiguration
-        if region == 'us-east-1':
+        if region == "us-east-1":
             result = s3_client.create_bucket(Bucket=bucket_name)
         else:
             result = s3_client.create_bucket(
                 Bucket=bucket_name,
-                CreateBucketConfiguration={'LocationConstraint': region}
+                CreateBucketConfiguration={"LocationConstraint": region},
             )
 
         if public:
             s3_client.put_public_access_block(
                 Bucket=bucket_name,
                 PublicAccessBlockConfiguration={
-                    'BlockPublicAcls': False,
-                    'IgnorePublicAcls': False,
-                    'BlockPublicPolicy': False,
-                    'RestrictPublicBuckets': False
-                }
+                    "BlockPublicAcls": False,
+                    "IgnorePublicAcls": False,
+                    "BlockPublicPolicy": False,
+                    "RestrictPublicBuckets": False,
+                },
             )
-            arn = [
-                f"arn:aws:s3:::{bucket_name}",
-                f"arn:aws:s3:::{bucket_name}/*"
-            ]
+            arn = [f"arn:aws:s3:::{bucket_name}", f"arn:aws:s3:::{bucket_name}/*"]
             policy = {
                 "Version": "2012-10-17",
                 "Statement": [
@@ -90,9 +96,9 @@ def create_bucket(s3_client, bucket_name, region, public=None):
                         "Action": [
                             "s3:*",
                         ],
-                        "Resource": arn
+                        "Resource": arn,
                     }
-                ]
+                ],
             }
             s3_client.put_bucket_policy(Bucket=bucket_name, Policy=json.dumps(policy))
 
@@ -102,6 +108,7 @@ def create_bucket(s3_client, bucket_name, region, public=None):
         print(f"Could not create bucket [{bucket_name}]")
         print(e)
         raise
+
 
 def main():
     bucket = ""
@@ -144,6 +151,7 @@ def main():
 
     if result:
         print(f"Bucket [{bucket}] created successfully.")
+
 
 if __name__ == "__main__":
     main()
--- aws-tools/s3-sync-buckets.py
+++ aws-tools/s3-sync-buckets.py
@@ -5,13 +5,15 @@ from botocore.exceptions import ClientError
 import sys
 import getopt
 
-regions=['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2']
-aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+regions = ["us-west-1", "us-west-2", "us-east-1", "us-east-2"]
+aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
 
 if aws_access_key_id == "" or aws_secret_access_key == "":
-  print("Please make sure that you set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables for AWS access")
-  exit()
+    print(
+        "Please make sure that you set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables for AWS access"
+    )
+    exit()
 
 
 # Remove 1st argument from the
@@ -24,97 +26,106 @@ options = "hs:t:r:"
 # Long options
 long_options = ["help", "source", "target", "region"]
 
+
 def usage():
-  msg = "Usage: python " + sys.argv[0] + "\n"  \
-        "Options: \n" \
-        " -s or --source \n" \
-        "    S3 source bucket name to copy contents from \n" \
-        "    Example: -s my-bucket \n" \
-        " -t or --target \n" \
-        "    S3 target bucket name to copy contents to \n" \
-        "    Example: -t my-other-bucket \n" \
-        " -r or --region \n" \
-        "    AWS region for bucket \n" \
-        "    Example: -r us-west-1 \n" \
-        " -h or --help \n" \
+    msg = (
+        "Usage: python " + sys.argv[0] + "\n"
+        "Options: \n"
+        " -s or --source \n"
+        "    S3 source bucket name to copy contents from \n"
+        "    Example: -s my-bucket \n"
+        " -t or --target \n"
+        "    S3 target bucket name to copy contents to \n"
+        "    Example: -t my-other-bucket \n"
+        " -r or --region \n"
+        "    AWS region for bucket \n"
+        "    Example: -r us-west-1 \n"
+        " -h or --help \n"
         "    Usage message"
-  print(msg)
-  exit()
+    )
+    print(msg)
+    exit()
 
 
 def getS3Session():
-  try:
-    #Creating Session With Boto3.
-    session = boto3.Session( 
-      aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    )
-    return session
-  except ClientError as e:
-    print("Could not get S3 Session")
-    print(e)
-    raise
-  
+    try:
+        # Creating Session With Boto3.
+        session = boto3.Session(
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        )
+        return session
+    except ClientError as e:
+        print("Could not get S3 Session")
+        print(e)
+        raise
+
+
 def getS3Resource():
-  try:
-    session = getS3Session()
-    #Creating S3 Resource From the Session.
-    s3 = session.resource('s3')
-    return s3
-  except ClientError as e:
-    print("Could not get S3 resource")
-    print(e)
-    raise
-  
+    try:
+        session = getS3Session()
+        # Creating S3 Resource From the Session.
+        s3 = session.resource("s3")
+        return s3
+    except ClientError as e:
+        print("Could not get S3 resource")
+        print(e)
+        raise
+
+
 def sync_buckets(s3, source_bucket, target_bucket):
-  try: 
-    srcbucket = s3.Bucket(source_bucket)
-    destbucket = s3.Bucket(target_bucket)
-    
-    # Iterate All Objects in Your S3 Bucket Over the for Loop
-    for file in srcbucket.objects.all():
-      copy_source = {
-        'Bucket': source_bucket,
-        'Key': file.key
-      }
-      
-      destbucket.copy(copy_source, file.key)
-      print(file.key +'- File Copied')
-    return True
-  except ClientError as e:
-    print("Could not sync buckets: [" + source_bucket + "] ==> [" + target_bucket + "]")
-    print(e)
-    raise
-  
+    try:
+        srcbucket = s3.Bucket(source_bucket)
+        destbucket = s3.Bucket(target_bucket)
+
+        # Iterate All Objects in Your S3 Bucket Over the for Loop
+        for file in srcbucket.objects.all():
+            copy_source = {"Bucket": source_bucket, "Key": file.key}
+
+            destbucket.copy(copy_source, file.key)
+            print(file.key + "- File Copied")
+        return True
+    except ClientError as e:
+        print(
+            "Could not sync buckets: ["
+            + source_bucket
+            + "] ==> ["
+            + target_bucket
+            + "]"
+        )
+        print(e)
+        raise
+
+
 def main():
     try:
         # Parsing argument
         arguments, values = getopt.getopt(argumentList, options, long_options)
-        
+
         if len(arguments) == 0:
             usage()
-            
+
         # checking each argument
-        for currentArgument, currentValue in arguments:            
+        for currentArgument, currentValue in arguments:
             if currentArgument in ("-h", "--help"):
                 usage()
                 break
             elif currentArgument in ("-s", "--source"):
-                print ("Source S3 Bucket: ", currentValue)
-                source_bucket=currentValue
+                print("Source S3 Bucket: ", currentValue)
+                source_bucket = currentValue
             elif currentArgument in ("-t", "--target"):
-                print ("Target S3 Bucket: ", currentValue)
-                target_bucket=currentValue
+                print("Target S3 Bucket: ", currentValue)
+                target_bucket = currentValue
     except getopt.error as err:
         # output error, and return with an error code
-        print (str(err))
+        print(str(err))
         usage()
         exit()
 
     if source_bucket == "":
         print("Missing required source bucket name")
         usage()
-        
+
     if target_bucket == "":
         print("Missing required target bucket name")
         usage()
@@ -123,9 +134,8 @@ def main():
     result = sync_buckets(s3, source_bucket, target_bucket)
 
     if result:
-        print ("Sync successful!")
+        print("Sync successful!")
 
-      
+
 if __name__ == "__main__":
     main()
-        
